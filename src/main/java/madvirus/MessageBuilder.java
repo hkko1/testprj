5772606c719ff2b0c0f0a498c5d1bcfcd60fe2f2
hkko1 <hyekyung.ko@telus.com>
2023-04-25 00:38:43 -0700

    test update

--- src/main/java/madvirus/MessageBuilder.java
+++ src/main/java/madvirus/MessageBuilder.java
@@ -13,7 +13,7 @@ public class MessageBuilder {
 
         } else {
 
-            result.append("Hello " + name);
+            result.append("Hello, hi. " + name);
 
         }
         return result.toString();

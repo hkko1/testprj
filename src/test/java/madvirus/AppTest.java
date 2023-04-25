a1285d4185957e4ec71cd51371566dc6e13c736f
hkko1 <hyekyung.ko@telus.com>
2023-04-25 06:57:33 -0700

    AppTest update to use jupiter

--- src/test/java/madvirus/AppTest.java
+++ src/test/java/madvirus/AppTest.java
@@ -1,38 +1,24 @@
 package madvirus;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+//import junit.framework.Test;
+//import junit.framework.TestCase;
+//import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+
+    @Test
+    public void testMessage() {
+
+        App obj = new App();
+        assertEquals("Hello hkko", obj.getMessage("hkko"));
+
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
